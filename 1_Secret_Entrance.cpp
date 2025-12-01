97fc3b7c69f0b78b289aaf70b0ae391655d79143
AhlgrenT <theodor17.ahlgren@gmail.com>
2025-12-01 21:42:30 +0100

    Day 1 complete

--- 1_Secret_Entrance.cpp
+++ 1_Secret_Entrance.cpp
@@ -5,20 +5,33 @@
 
 int main(int argc, char* argv[]) {
 
-    std::ifstream input(argv[1]);
+  std::ifstream input(argv[1]);
 
 	if (!input) {
-		  std::cerr << "Could not open file!" << std::endl;
-		  return 1;
+		std::cerr << "Could not open file!" << std::endl;
+		return 1;
 	}
 	
 	std::string line;
+	int dial = 50;
+	int zeroes = 0;
 	
 	while (std::getline(input, line)) {
-    std::cout << line << std::endl;
+    if (line.empty()) continue;
+    
+		int value = std::stoi(line.substr(1)); 
+
+    if (line.at(0) == 'L') {
+    	value = 100 - value;
+    }
+ 
+    dial = (dial + value) % 100;
+    if (dial == 0) zeroes++;
 	}
 	
+	std::cout << zeroes << std::endl;
 	input.close();
+	
 	return 0;
 }
 
